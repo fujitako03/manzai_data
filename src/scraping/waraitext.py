cbc4c7c11b8dcb2ba70cc1d8975d2c3c2c28e1b2
fujitako03 <thisis.snsd03@gmail.com>
2022-01-14 23:31:55 +0900

    feat get manzai title

--- src/scraping/waraitext.py
+++ src/scraping/waraitext.py
@@ -1,3 +1,5 @@
+import re
+
 import bs4
 
 from scraping_base import ScrapingBase
@@ -5,9 +7,24 @@ from scraping_base import ScrapingBase
 
 class WaraiTextManzai(ScrapingBase):
     def __init__(self, url):
+        super().__init__(url, weit_sec=1)
         self.url=url
         self.page_soup=self.get_soup()
         pass
 
-    def _get_title(self):
-        self.page_soup.
+    def _get_title(self) -> str:
+        """漫才のタイトルを抽出
+
+        Returns:
+            str: 漫才タイトル
+        """
+        page_title = self.page_soup.select_one("title").get_text()
+        manzai_title = re.findall("(?<=【).+?(?=】)", page_title)[0]
+
+        return manzai_title
+
+if __name__=='__main__':
+    wt = WaraiTextManzai(
+        url="https://waraitext.com/post-75/"
+    )
+    wt._get_title()
