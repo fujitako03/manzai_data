0d372234253a9a4573066a169755d30d93c32563
fujitako03 <thisis.snsd03@gmail.com>
2022-01-15 18:37:32 +0900

    test performer, neta_type

--- tests/scraping/test_waraitext.py
+++ tests/scraping/test_waraitext.py
@@ -10,3 +10,21 @@ def test_get_title():
     title = wt._get_title()
     
     assert title == '暴走族'
+
+def test_get_performer():
+    test_url = "https://waraitext.com/post-75/"
+    wt = WaraiTextManzai(
+        url=test_url
+    )
+    manzai_shi = wt._get_performer()
+    
+    assert manzai_shi == '紳助竜介'
+
+def test_get_neta_type():
+    test_url = "https://waraitext.com/post-75/"
+    wt = WaraiTextManzai(
+        url=test_url
+    )
+    neta_type = wt._get_neta_type()
+    
+    assert neta_type == '漫才'
