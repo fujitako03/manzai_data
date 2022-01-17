28816fdabc3ef8c6c3166b66772542e3261468c7
fujitako03 <thisis.snsd03@gmail.com>
2022-01-17 21:30:45 +0900

    feat add _get_neta_text()

--- src/scraping/waraitext.py
+++ src/scraping/waraitext.py
@@ -38,7 +38,7 @@ class WaraiTextManzai(ScrapingBase):
         return performer
 
     def _get_title(self) -> str:
-        """漫才のタイトルを抽出
+        """ネタのタイトルを抽出
 
         Returns:
             str: 漫才タイトル
@@ -47,10 +47,70 @@ class WaraiTextManzai(ScrapingBase):
         manzai_title = re.findall("(?<=【).+?(?=】)", page_title)[0]
 
         return manzai_title
+
+    def _get_neta_text(self) -> str:
+        """ネタの文章を抽出
+
+        Returns:
+            str: ネタの文章
+        """
+        # 関連するブロックを取得
+        neta_content = self.page_soup.select_one("div.entry-content")
+
+        # 不要なタグを削除
+        neta_content = self._clean_neta_content(neta_content)
+
+        # pタグを取得
+        neta_p = neta_content.select("p")
+
+        # pタグごとに処理
+        neta_text_raw = "\n".join([
+            self._clean_neta_ptag(p)
+            for p
+            in neta_p
+            if self._clean_neta_ptag(p)])
+
+        return neta_text_raw
+    
+    def _clean_neta_content(self, content):
+        """ネタのブロックを前処理する
+        """
+        # プロフィールを削除
+        content.find("h2").extract()
+        content.find("blockquote").extract()
+        content.find("script").extract()
+
+        return content
+    
+    def _clean_neta_ptag(self, p_tag):
+        """ネタのpタグを前処理する
+
+        Args:
+            p_tag (Tag): p_tag
+
+        Returns:
+            Tag: p_tag
+        """
+        p_text = p_tag.get_text()
+
+        # 改行を削除
+        p_text = p_text.replace("\n", "")
+
+        # 関連情報タグの場合Noneを返す
+        if p_text.startswith("【関連】"):
+            return None
+
+        # 空のタグの場合Noneを返す
+        elif p_text == "":
+            return None
+
+        else:
+            return p_text
     
 if __name__=='__main__':
     wt = WaraiTextManzai(
         url="https://waraitext.com/post-75/"
     )
-    wt._get_title()
-    wt._get_manzai_shi()
+    wt._get_neta()
+    # wt._get_title()
+    # wt._get_manzai_shi()
