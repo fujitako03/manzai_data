9f0eca3f3a00eec517a29d0abc75283a453c4c8d
fujitako03 <thisis.snsd03@gmail.com>
2022-01-22 16:59:01 +0900

    add argparse

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import argparse
+
 from src.scraping.waraitext import WaraiTextCrawler
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('start', metavar='start_index', type=int,
+                    help='Start index (page url post-x)')
+parser.add_argument('-n', '--num', default=1, metavar='neta_num', type=int,
+                    help='Number of Neta to get.')
+args = parser.parse_args()  
 
 def main():
     crawler = WaraiTextCrawler(
-        start_index=1,
-        neta_num=3,
+        start_index=args.start,
+        neta_num=args.num,
     )
     crawler.main()
 
